3b5f5cd9734136e33ac76bea7e42413446a7e0fe
joo1712 <jxt302@naver.com>
2020-08-17 11:38:08 +0900

    Git hub Test

---
+++ spring-boot-mybatis-demo/src/main/java/me/eastglow/SpringBootMybatisDemoApplication.java
@@ -0,0 +1,13 @@
+package me.eastglow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisDemoApplication.class, args);
+	}
+
+}
---
+++ spring-boot-mybatis-demo/src/test/java/me/eastglow/SpringBootMybatisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.eastglow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMybatisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
